46f0cebe30dfa028a3c4f857d03506296ce98eed
Stackbit Code Editor <support@stackbit.com>
2021-02-15 10:10:43 +0000

    SectionReviews.js: changed

--- src/components/SectionReviews.js
+++ src/components/SectionReviews.js
@@ -13,7 +13,7 @@ export default class SectionReviews extends React.Component {
                 <h2 className="block-title">{_.get(section, 'title', null)}</h2>
                 )}
                 {_.get(section, 'subtitle', null) && (
-                <p className="block-subtitle">Review subtitle prefix:<br/><br/>
+                <p className="block-subtitle">Review subtitle prefix<br/><br/>
                   {htmlToReact(_.get(section, 'subtitle', null))}
                 </p>
                 )}
